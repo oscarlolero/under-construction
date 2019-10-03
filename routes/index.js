b83989bf4f2b18e2e3135a3177cda8b0612b1808
Oscar Montes <oscmcojc1@live.com.mx>
2019-10-02 21:24:33 -0500

    Downloadable content

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
+var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+router.get('/download', function(req, res, next) {
+  const file = `${__dirname}/../public/download/${req.query.song}.${req.query.format}`;
+  res.download(file);
+});
+
 
 module.exports = router;
